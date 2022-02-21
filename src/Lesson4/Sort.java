4871431abebc68ad85c6cbbdf6e1bd4a9b745808
yua375 <yua@tut.by>
2022-02-21 21:25:55 +0300

    Сортировка пузырьком

---
+++ src/Lesson4/Sort.java
@@ -0,0 +1,19 @@
+package Lesson4;
+
+import java.util.Arrays;
+public class Sort {
+    public static void main(String[] args) {
+
+        int[] array = new int[]{2,3,1,4,0,-2};
+        for (int i =0; i < array.length; i++) {
+            for (int j = array.length -1; j > i; j--) {
+                if (array[j - 1] > array[j]) {
+                        int buf = array[j];
+                        array[j] = array[j - 1];
+                        array [j - 1] = buf;
+                }
+                System.out.println(Arrays.toString(array));
+            }
+        }
+    }
+}
